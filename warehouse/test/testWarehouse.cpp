c4242f3363012cfb40c4606b57b57e8259889383
Arantxa Madamsir <arantxa.madamsir@student.hu.nl>
2023-05-25 04:10:11 +0200

    Write testcases (incomplete)

--- warehouse/test/testWarehouse.cpp
+++ warehouse/test/testWarehouse.cpp
@@ -1,7 +1,7 @@
-#include "include/catch.hpp"
-
-#include "../src/include/warehouse.hpp"
 #include <iostream>
+#include "include/catch.hpp"
+#include "../src/include/warehouse.hpp"
+
 
 ///////////////////////////////////////////////////////////////
 //                                                           //
@@ -10,68 +10,15 @@
 ///////////////////////////////////////////////////////////////
 //                                                           //
 // This file contains all testcases for the Warehouse class. //
-// Currently it only contains tests for the rearranging of   //
-// shelves. All other Warehouse functions will also need to  //
-// be tested. You will have to do this yourself.             //
-//                                                           //
-// For information on how to write testcases with catch2,    //
-// see https://github.com/catchorg/Catch2.                   //
 //                                                           //
 ///////////////////////////////////////////////////////////////
-
-
-///////////////////////////////////////////////////////////////
-//                Functions used in testcases                //
-///////////////////////////////////////////////////////////////
-
-/// \brief 
-/// Creates a mock Warehouse.
-/// \return Warehouse with shelf of books.
-/// \details
-/// Creates a Warehouse with 3 unsorted shelves with a total of 100 Books, 60 Boxes, and 60 Toy Bears.
-/// \n Adds forklift certified employee Bob.
-/// \n This function can be used in testcases to avoid the repeated construction of Warehouse objects.
-Warehouse createMockWarehouse(){
-    Warehouse warehouse = Warehouse();
-    Shelf shelf1 = Shelf();
-    shelf1.pallets = {
-        Pallet("Books", 100, 40), 
-        Pallet("Boxes", 100, 10), 
-        Pallet("Books", 100, 20), 
-        Pallet("Books", 100, 20)
-    };
-
-    Shelf shelf2 = Shelf();
-    shelf2.pallets = {
-        Pallet("Books", 100, 15), 
-        Pallet("Boxes", 100, 20), 
-        Pallet("Books", 100, 5), 
-        Pallet("Boxes", 100, 30)
-    };
-
-    Shelf shelf3 = Shelf();
-    shelf3.pallets = {
-        Pallet("Toy Bears", 100, 20), 
-        Pallet("Toy Bears", 100, 10), 
-        Pallet(), 
-        Pallet("Toy Bears", 100, 30)
-    };
-    
-    warehouse.addShelf(shelf1);
-    warehouse.addShelf(shelf2);
-    warehouse.addShelf(shelf3);
-
-    warehouse.addEmployee(Employee("Bob", true));
-
-    return warehouse;
-}
 
 
 ///////////////////////////////////////////////////////////////
 //           Warehouse::rearrangeShelf test cases            //
 ///////////////////////////////////////////////////////////////
 
-TEST_CASE("Rearrange empty shelf", "Warehouse::rearrangeShelf"){
+TEST_CASE("Rearrange empty shelf", "Warehouse::rearrangeShelf") {
     // Construct empty warehouse and add empty shelf and forklift certified Employee.
     Warehouse warehouse = Warehouse();
     warehouse.addShelf(Shelf());
@@ -97,7 +44,7 @@ TEST_CASE("Rearrange empty shelf", "Warehouse::rearrangeShelf"){
     REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 0);
 }
 
-TEST_CASE("Rearrange full shelf", "Warehouse::rearrangeShelf"){
+TEST_CASE("Rearrange full shelf", "Warehouse::rearrangeShelf") {
     // Construct warehouse with unsorted shelf of books.
     Warehouse warehouse = Warehouse();
     Shelf shelf1 = Shelf();
@@ -131,7 +78,7 @@ TEST_CASE("Rearrange full shelf", "Warehouse::rearrangeShelf"){
     REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 40);
 }
 
-TEST_CASE("Rearrange half filled shelf", "Warehouse::rearrangeShelf"){
+TEST_CASE("Rearrange half filled shelf", "Warehouse::rearrangeShelf") {
     // Construct empty warehouse and unsorted shelf of books.
     Warehouse warehouse = Warehouse();
     Shelf shelf1 = Shelf();
@@ -165,7 +112,7 @@ TEST_CASE("Rearrange half filled shelf", "Warehouse::rearrangeShelf"){
     REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 40);
 }
 
-TEST_CASE("Rearrange shelf without qualified employee", "Warehouse::rearrangeShelf"){
+TEST_CASE("Rearrange shelf without qualified employee", "Warehouse::rearrangeShelf") {
     // Construct warehouse with unsorted shelf of books.
     Warehouse warehouse = Warehouse();
     Shelf shelf1 = Shelf();
@@ -199,7 +146,7 @@ TEST_CASE("Rearrange shelf without qualified employee", "Warehouse::rearrangeShe
     REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 10);
 }
 
-TEST_CASE("Rearrange shelf with quallified, but busy, employee", "Warehouse::rearrangeShelf"){
+TEST_CASE("Rearrange shelf with qualified, but busy, employee", "Warehouse::rearrangeShelf") {
     // Construct warehouse with unsorted shelf of books.
     Warehouse warehouse = Warehouse();
     Shelf shelf1 = Shelf();
@@ -234,4 +181,123 @@ TEST_CASE("Rearrange shelf with quallified, but busy, employee", "Warehouse::rea
     REQUIRE(warehouse.shelves[0].pallets[1].getItemCount() == 40);
     REQUIRE(warehouse.shelves[0].pallets[2].getItemCount() == 30);
     REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 10);
+}
+
+
+///////////////////////////////////////////////////////////////
+//           Warehouse::pickItems test cases                 //
+///////////////////////////////////////////////////////////////
+
+TEST_CASE("Pick items when there aren't enough available", "Warehouse::pickItems") {
+    // Construct warehouse with shelves of books and toys.
+    Warehouse warehouse = Warehouse();
+    Shelf shelf1 = Shelf();
+    shelf1.pallets = {
+        Pallet("Toys", 100, 20), 
+        Pallet("Toys", 100, 5), 
+        Pallet("Books", 100, 40), 
+        Pallet("Books", 100, 10)
+    };
+
+    Shelf shelf2 = Shelf();
+    shelf2.pallets = {
+        Pallet("Toys", 100, 10), 
+        Pallet("Books", 100, 5),
+        Pallet("Books", 100, 10), 
+        Pallet()
+    };
+
+    warehouse.addShelf(shelf1);
+    warehouse.addShelf(shelf2);
+
+    // Check if shelves have been picked from.
+    // These shelves should not already have been picked from.
+    REQUIRE(warehouse.shelves[0].pallets[0].getItemCount() == 20);
+    REQUIRE(warehouse.shelves[0].pallets[1].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[0].pallets[2].getItemCount() == 40);
+    REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 10);
+
+    REQUIRE(warehouse.shelves[1].pallets[0].getItemCount() == 10);
+    REQUIRE(warehouse.shelves[1].pallets[1].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[1].pallets[2].getItemCount() == 10);
+    REQUIRE(warehouse.shelves[1].pallets[3].getItemCount() == 0);
+
+    // Pick 40 toys from warehouse when there are only 35 available.
+    bool successful1 = warehouse.pickItems("Toys", 40);
+    // Should fail
+    REQUIRE(!successful1);
+
+    // Pick 70 books from warehouse when there are only 65 available.
+    bool successful2 = warehouse.pickItems("Books", 70);
+    // Should fail
+    REQUIRE(!successful2);
+
+    // Check if shelves have been picked from.
+    // These shelves should not have been picked from.
+    REQUIRE(warehouse.shelves[0].pallets[0].getItemCount() == 20);
+    REQUIRE(warehouse.shelves[0].pallets[1].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[0].pallets[2].getItemCount() == 40);
+    REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 10);
+
+    REQUIRE(warehouse.shelves[1].pallets[0].getItemCount() == 10);
+    REQUIRE(warehouse.shelves[1].pallets[1].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[1].pallets[2].getItemCount() == 10);
+    REQUIRE(warehouse.shelves[1].pallets[3].getItemCount() == 0);
+}
+
+TEST_CASE("Pick items when there are enough available", "Warehouse::pickItems") {
+    // Construct warehouse with shelves of books and toys.
+    Warehouse warehouse = Warehouse();
+    Shelf shelf1 = Shelf();
+    shelf1.pallets = {
+        Pallet("Toys", 100, 20), 
+        Pallet("Toys", 100, 5), 
+        Pallet("Books", 100, 40), 
+        Pallet("Books", 100, 10)
+    };
+
+    Shelf shelf2 = Shelf();
+    shelf2.pallets = {
+        Pallet("Toys", 100, 10), 
+        Pallet("Books", 100, 5),
+        Pallet("Books", 100, 10), 
+        Pallet()
+    };
+
+    warehouse.addShelf(shelf1);
+    warehouse.addShelf(shelf2);
+
+    // Check if shelves have been picked from.
+    // These shelves should not already have been picked from.
+    REQUIRE(warehouse.shelves[0].pallets[0].getItemCount() == 20);
+    REQUIRE(warehouse.shelves[0].pallets[1].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[0].pallets[2].getItemCount() == 40);
+    REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 10);
+
+    REQUIRE(warehouse.shelves[1].pallets[0].getItemCount() == 10);
+    REQUIRE(warehouse.shelves[1].pallets[1].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[1].pallets[2].getItemCount() == 10);
+    REQUIRE(warehouse.shelves[1].pallets[3].getItemCount() == 0);
+
+    // Pick 35 toys from warehouse when there are 35 available.
+    bool successful1 = warehouse.pickItems("Toys", 35);
+    // Should be successful
+    REQUIRE(successful1);
+
+    // Pick 60 books from warehouse when there are 65 available.
+    bool successful2 = warehouse.pickItems("Books", 60);
+    // Should be successful
+    REQUIRE(successful2);
+
+    // Check if shelves have been picked from.
+    // These shelves should not have been picked from.
+    REQUIRE(warehouse.shelves[0].pallets[0].getItemCount() == 0);
+    REQUIRE(warehouse.shelves[0].pallets[1].getItemCount() == 0);
+    REQUIRE(warehouse.shelves[0].pallets[2].getItemCount() == 0);
+    REQUIRE(warehouse.shelves[0].pallets[3].getItemCount() == 0);
+
+    REQUIRE(warehouse.shelves[1].pallets[0].getItemCount() == 0);
+    REQUIRE(warehouse.shelves[1].pallets[1].getItemCount() == 0);
+    REQUIRE(warehouse.shelves[1].pallets[2].getItemCount() == 5);
+    REQUIRE(warehouse.shelves[1].pallets[3].getItemCount() == 0);
 }
