83b66f3ff6c3fc3d8c1678e59048b6db9f903985
Arantxa Madamsir <arantxa.madamsir@student.hu.nl>
2023-05-26 16:38:41 +0200

    Edit code format

--- warehouse/src/shelf.cpp
+++ warehouse/src/shelf.cpp
@@ -1,7 +1,8 @@
 #include "include/shelf.hpp"
 
 
-Shelf::Shelf(): pallets({Pallet(), Pallet(), Pallet(), Pallet()}) {} 
+Shelf::Shelf(): 
+    pallets({Pallet(), Pallet(), Pallet(), Pallet()}) {} 
 
 bool Shelf::swapPallet(int slot, int slot2) { // Test if method works properly without = 
     if (slot < 0 or slot >= pallets.size() or slot2 < 0 or slot2 >= pallets.size()) {
